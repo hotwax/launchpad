fea7a916261f72d758dca4c468eb305b162096d7
k2maan <ketuman333@gmail.com>
2023-08-02 15:41:41 +0530

    Improved: flow for accessing login route and added launchpad logout on app logout

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,17 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import Home from '@/views/Home.vue';
 import Login from '@/views/Login.vue';
+import { useAuthStore } from "@/store/auth";
+
+const loginGuard = (to: any, from: any, next: any) => {
+  const authStore = useAuthStore()
+  if (authStore.isAuthenticated) {
+    // if route has redirectUrl, stop it there for showing session confirmation popup
+    if (to.query?.redirectUrl) next()
+    else next('/home')
+  }
+  next();
+};
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -17,6 +28,7 @@ const routes: Array<RouteRecordRaw> = [
     path: '/login',
     name: 'Login',
     component: Login,
+    beforeEnter: loginGuard
   }
 ];
 
